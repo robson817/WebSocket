f63fac406775bd3a64a5d77ba59af4b7c1b9c4bd
Robson Alan Mantovani <robsonamantovani@gmail.com>
2025-03-26 17:59:16 -0300

    projeto concluido

---
+++ servidor/db/dbConnect.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const cliente = new MongoClient(process.env.MONGO_URI);
+
+let documentosColecao, usuariosColecao;
+
+try {
+  await cliente.connect();
+
+  const db = cliente.db("alura-websockets");
+  documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios");
+
+  console.log("Conectado ao banco de dados com sucesso!");
+} catch (erro) {
+  console.log(erro);
+}
+
+export { documentosColecao, usuariosColecao };
