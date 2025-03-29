3f0dacfc1af729784ac8d00438c626c960ae840c
Robson Alan Mantovani <robsonamantovani@gmail.com>
2025-03-29 18:30:55 -0300

    Adicionado mensagem no console para acompanhar os eventos de login.

--- servidor/registrarEventos/login.js
+++ servidor/registrarEventos/login.js
@@ -3,23 +3,28 @@ import autenticarUsuario from "../utils/autenticarUsuario.js";
 import gerarJwt from "../utils/gerarJwt.js";
 
 function registrarEventosLogin(socket, io) {
-  socket.on("autenticar_usuario", async ({ nome, senha }) => {
-    const usuario = await encontrarUsuario(nome);
+    socket.on("autenticar_usuario", async ({ nome, senha }) => {
+        console.log("Evento autenticar_usuario chamado com:", { nome, senha });
 
-    if (usuario) {
-      const autenticado = autenticarUsuario(senha, usuario);
+        const usuario = await encontrarUsuario(nome);
 
-      if (autenticado) {
-        const tokenJwt = gerarJwt({ nomeUsuario: nome });
+        if (usuario) {
+            const autenticado = autenticarUsuario(senha, usuario);
 
-        socket.emit("autenticacao_sucesso", tokenJwt);
-      } else {
-        socket.emit("autenticacao_erro");
-      }
-    } else {
-      socket.emit("usuario_nao_encontrado");
-    }
-  });
+            if (autenticado) {
+                const tokenJwt = gerarJwt({ nomeUsuario: nome });
+
+                console.log("Autenticação bem-sucedida para o usuário:", nome);
+                socket.emit("autenticacao_sucesso", tokenJwt);
+            } else {
+                console.log("Erro de autenticação: senha inválida para o usuário:", nome);
+                socket.emit("autenticacao_erro");
+            }
+        } else {
+            console.log("Usuário não encontrado:", nome);
+            socket.emit("usuario_nao_encontrado");
+        }
+    });
 }
 
 export default registrarEventosLogin;
