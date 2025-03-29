742f0e9d2de6c10da97651a0bf51887bce9a327b
Robson Alan Mantovani <robsonamantovani@gmail.com>
2025-03-29 18:42:11 -0300

    Retorno a versão original do diretório público.

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -9,8 +9,8 @@ import "./db/dbConnect.js";
 const app = express();
 const porta = process.env.PORT || 3000;
 
-// const caminhoAtual = url.fileURLToPath(import.meta.url);
-const diretorioPublico = path.join(process.cwd(), "public");
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(express.static(diretorioPublico));
 
 const servidorHttp = http.createServer(app);
